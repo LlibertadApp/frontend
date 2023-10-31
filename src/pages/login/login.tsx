92d24f563f06a516c8956927ca4d5a981d689afb
bonino97 <juanbonino97@gmail.com>
2023-10-31 13:26:57 +0100

    Validation in input

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,6 +1,8 @@
 import { useNavigate, Link } from 'react-router-dom';
 import { useFormik } from 'formik';
 import { observer } from 'mobx-react-lite';
+import * as yup from 'yup';
+
 import Button from '#/components/button';
 import Input from '#/components/input';
 import { SnackBar } from '#/components/snackbar';
@@ -25,18 +27,38 @@ const LoginPage: React.FC = () => {
     navigate('/dashboard');
   };
 
-  const { handleSubmit, handleChange } = useFormik({
-    initialValues: {
-      dni: '',
-      password: '',
-    },
-    onSubmit,
+  const validationSchema = yup.object({
+    dni: yup.string().required('Campo requerido'),
+    password: yup.string().required('Campo requerido'),
   });
+
+  const { handleSubmit, handleBlur, handleChange, errors, touched } = useFormik(
+    {
+      initialValues: {
+        dni: '',
+        password: '',
+      },
+      onSubmit,
+      validationSchema,
+    },
+  );
 
   const handleClick = async () => {
     // Maneja la lógica cuando se hace clic en el botón
     return;
   };
+
+  function dniChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value;
+    const cleanValue = value.replace(/[^\d]/g, '').slice(0, 8);
+    if (!cleanValue) {
+      e.target.value = cleanValue;
+    } else {
+      const formatted = parseInt(cleanValue).toLocaleString('es-AR');
+      e.target.value = formatted;
+    }
+    handleChange(e);
+  }
 
   return (
     <section className="relative flex flex-col items-center h-screen overflow-hidden bg-gray-100">
@@ -62,7 +84,9 @@ const LoginPage: React.FC = () => {
               type="text"
               id="dni"
               placeholder="Ingresa tu DNI"
-              onChange={handleChange}
+              onChange={dniChange}
+              onBlur={handleBlur}
+              error={!!errors.dni && !!touched.dni}
             />
           </div>
           <div className="flex items-center mb-6 text-lg md:mb-8 shadow-3xl">
@@ -72,6 +96,8 @@ const LoginPage: React.FC = () => {
               id="password"
               placeholder="Ingresa tu Contraseña"
               onChange={handleChange}
+              onBlur={handleBlur}
+              error={!!errors.password && !!touched.password}
             />
           </div>
           <div className="flex flex-col items-center text-lg">
