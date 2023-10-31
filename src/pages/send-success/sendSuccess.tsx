62a9caf0b8f98ab3037dbee568e7f4c186c7c758
n1-n2-n3 <xiomaraengine@gmail.com>
2023-10-31 03:26:13 -0300

    Vista send-success actualizada segun maquetadoUI

--- src/pages/send-success/sendSuccess.tsx
+++ src/pages/send-success/sendSuccess.tsx
@@ -2,48 +2,42 @@ import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import { observer } from 'mobx-react';
 import Button from '#/components/button';
-import FormHeader from '#/components/formHeader';
-import ProgressIndicator from '#/components/progressIndicator';
-import { ProgressStepStatus } from '#/components/progressIndicator/types';
 import { ISendSuccessProps } from './types';
 import './styles.css';
+import Navbar from '#/components/navbar';
 
 const SendSuccessPage: FC<ISendSuccessProps> = ({ message }) => {
   return (
-    <section className="bg-gray-100 items-center flex flex-col ">
-      <FormHeader routerLink="/" title="Datos enviados con éxito" />
+    <section className="bg-gray-100 items-center flex flex-col w-full">
+      <Navbar />
       <div className="p-4">
-        <div className="container mx-auto">
-          <div className="flex items-center justify-center my-210">
-            <ProgressIndicator
-              steps={[
-                ProgressStepStatus.Successful,
-                ProgressStepStatus.Successful,
-                ProgressStepStatus.Successful,
-              ]}
-            />
-          </div>
-          <div className="flex items-center justify-center my-20 ">
+        <div className="container mx-auto py-20 w-full">
+          <div className="flex items-center justify-center my-14">
             <img
-              src="src/assets/images/square-logo.svg"
+              src="src/assets/icon/success.svg"
               alt="data sent successful"
               className="object-cover rounded w-68 h-auto"
             />
           </div>
-          <div className="flex items-center justify-center my-20">
+          <div className="flex items-center justify-center py-4">
             <h3 className="successfull">
-              {message ?? '¡ MUCHAS GRACIAS POR FISCALIZAR !'}
+              {message ?? '¡Datos enviados con éxito!'}
             </h3>
           </div>
+              <h3 className="flex text-start text-base justify-center">
+                Muchas gracias por fiscalizar, VLLC!
+              </h3>
           <div className="flex items-center justify-center my-20">
             {/* TODO: Mover a Dashboard */}
-            <Link to="/">
+            <div className="w-full mx-6">
+              <Link to="/">
               <Button
-                className="bg-violet-brand p-4 text-white w-full rounded-xl font-semibold text-xl tracking-wider"
+                className="bg-violet-brand p-4 text-white rounded-xl font-semibold text-xl tracking-wider w-full cursor-default"
                 type="submit"
                 label="Volver a inicio"
-              />
-            </Link>
+                />
+                </Link>
+            </div>
           </div>
         </div>
       </div>
