8ae0490c5be0cbcbc0b428d2a1fc7e261ac5edf7
Nicolas Wolf <nico.wolf@outlook.com>
2023-11-01 20:03:42 -0300

    add overlay component

---
+++ src/components/overlay/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { OverlayProps } from './types'
+import { useHamburgerMenu } from '#/context/HamburgerContext'
+
+const Overlay: React.FC<OverlayProps> = ({children}) => {
+
+const {menuOpen} = useHamburgerMenu()
+  return (
+    <section>
+        {
+            menuOpen &&
+            <div className='w-full h-full top-0 bottom-0 absolute backdrop-blur-[2px] bg-overlay'></div>
+        }
+      {children}
+    </section>
+  )
+}
+
+export default Overlay
