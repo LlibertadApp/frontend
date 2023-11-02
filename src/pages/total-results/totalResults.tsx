5730e71888cd9433b7ced7e2cddbeaf90edef9c0
juampymdd <juampymdd@gmail.com>
2023-11-02 20:33:54 -0300

    Se agrega fullwith para el boton filtro cuando no hay filtros seleccionados

--- src/pages/total-results/totalResults.tsx
+++ src/pages/total-results/totalResults.tsx
@@ -23,8 +23,8 @@ const TotalResultsPage = () => {
           Resultados totales
         </p>
         {/* Sección de botones */}
-        <section className="grid grid-cols-2 gap-4">
-        <ButtonClearFilter amountOfFilters={filters.length} clearFilters={clearFilters} />
+        <section className="flex justify-center gap-4">
+        {filters.length > 0 && <ButtonClearFilter amountOfFilters={filters.length} clearFilters={clearFilters} />}
         <ButtonFilter amount={filters.length} />
         </section>
         {/* Lista de filtros */}
