94733d6324e8d0a29b15b2870514fc7f0742d192
bonino97 <juanbonino97@gmail.com>
2023-11-03 23:28:04 +0100

    Fix margin

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -37,7 +37,7 @@ const HomePage = () => {
               />
               <CardLink
                 link={paths.irregularities}
-                text={'Denunciar Irregularidades'}
+                text={'Denunciar irregularidades'}
                 icon={'hand-speaker.svg'}
                 color={colors.Red}
               />
