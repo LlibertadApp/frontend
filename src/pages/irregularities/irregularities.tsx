acfd0db3fb367046e1bd65567add4faa109daa73
bonino97 <juanbonino97@gmail.com>
2023-11-03 23:23:59 +0100

    Fixes

--- src/pages/irregularities/irregularities.tsx
+++ src/pages/irregularities/irregularities.tsx
@@ -7,11 +7,11 @@ import { observer } from 'mobx-react';
 const IrregularitiesPage = () => {
   return (
     <>
-      <Navbar routerLink="/home" />
+      <Navbar routerLink={paths.home} />
       <main className="w-full flex justify-center">
         <section className="md:w-1/2 w-11/12 shadow-3xl rounded-xl py-8 flex flex-col items-center space-y-8">
           <h2 className="text-red uppercase text-4xl font-black">
-            irregularidades
+            IRREGULARITIES
           </h2>
 
           <article className="w-full flex flex-col justify-start space-y-2">
@@ -25,10 +25,10 @@ const IrregularitiesPage = () => {
               color={colors.Red}
             />
           </article>
-          
+
           <article className="w-full flex flex-col justify-start space-y-2">
             <span className="text-left w-full font-bold">
-                Denunciar irregularidades
+              Denunciar irregularidades
             </span>
             <CardLink
               link={paths.irregularities}
