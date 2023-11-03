3e26aeae9d9c8e27f9ba981d8d798be27f13a5a1
Santiago de Nicolás <santidenicolas@gmail.com>
2023-11-03 02:38:59 -0300

    destructuring button props

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,9 +1,9 @@
 // import React from 'react';
 // import { IButtonProps } from './types';
 
-import classNames from "classnames";
-import { ButtonAppearance, IButtonProps } from "./types";
-import { LoadingIndicator } from "../loadingIndicator";
+import classNames from 'classnames';
+import { ButtonAppearance, IButtonProps } from './types';
+import { LoadingIndicator } from '../loadingIndicator';
 
 // const Button: React.FC<IButtonProps> = ({
 //   type,
@@ -24,22 +24,35 @@ import { LoadingIndicator } from "../loadingIndicator";
 
 // export default Button;
 
-export default function Button(props: IButtonProps) {
+export default function Button({
+  appearance = 'filled',
+  type,
+  onClick,
+  disabled,
+  label,
+  children,
+  className,
+}: IButtonProps) {
   const appereances: Record<ButtonAppearance, string> = {
-    filled: "bg-violet-brand text-white",
-    outlined: "border border-violet-brand text-violet-brand",
-    ghost: "text-violet-brand",
-  }
+    filled: 'bg-violet-brand text-white',
+    outlined: 'border border-violet-brand text-violet-brand',
+    ghost: 'text-violet-brand',
+  };
 
-  const buttonAppereance = appereances[props.appearance || "filled"];
+  const buttonAppereance = appereances[appearance];
 
   return (
     <button
-      type={props.type}
-      onClick={props.onClick}
-      disabled={props.disabled}
-      className={classNames("w-full p-[18px] text-lg font-medium rounded-xl flex flex-row gap-[10px] justify-center items-center", buttonAppereance, props.className)}>
-      {props.children || props.label}
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+      className={classNames(
+        'w-full p-[18px] text-lg font-medium rounded-xl flex flex-row gap-[10px] justify-center items-center',
+        buttonAppereance,
+        className,
+      )}
+    >
+      {children || label}
     </button>
   );
 }
