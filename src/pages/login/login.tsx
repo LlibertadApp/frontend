f3ae9871a882b7dd82a96332861283b8ac56094b
bonino97 <juanbonino97@gmail.com>
2023-11-05 07:23:00 +0100

    Change placeholder

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -41,81 +41,105 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <main className='px-4 mt-[50px] mx-auto flex flex-col items-center'>
+    <main className="px-4 mt-[50px] mx-auto flex flex-col items-center max-w-md">
       <img
-        src='assets/logos/fenix-new-bg.svg'
-        alt='fenix'
-        className='object-cover h-auto w-28 mb-10' />
+        src="assets/logos/fenix-new-bg.svg"
+        alt="fenix"
+        className="object-cover h-auto w-28 mb-10"
+      />
 
-      <h1 className='text-[32px] font-light text-center white-space: pre-line'>
-        Entre todos, <br/><strong className='text-violet-brand font-semibold break-words'>evitemos el fraude.</strong>
+      <h1 className="text-[32px] font-light text-center white-space: pre-line">
+        Entre todos, <br />
+        <strong className="text-violet-brand font-semibold break-words">
+          evitemos el fraude.
+        </strong>
       </h1>
 
-      <Formik initialValues={initialValues} onSubmit={onLogin} validationSchema={validationSchema}>
-        {
-          ({values, handleChange, handleBlur, setFieldValue, handleSubmit, errors}) => (
-            <Form className='w-full mt-16 flex flex-col gap-6' onSubmit={handleSubmit}>
-              <TextField
-                InputLabelProps={{ style: { fontFamily: 'Poppins' }}}
-                InputProps={{ style: { borderRadius: '8px', fontFamily: 'Poppins' } }}
-                sx={{ width: '100%' }}
-                
-                id="email"
-                value={values.email}
-                onChange={handleChange}
-                onBlur={handleBlur} 
-                error={Boolean(errors.email)}
-                helperText={errors.email}
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onLogin}
+        validationSchema={validationSchema}
+      >
+        {({
+          values,
+          handleChange,
+          handleBlur,
+          setFieldValue,
+          handleSubmit,
+          errors,
+        }) => (
+          <Form
+            className="w-full mt-16 flex flex-col gap-6"
+            onSubmit={handleSubmit}
+          >
+            <TextField
+              InputLabelProps={{ style: { fontFamily: 'Poppins' } }}
+              InputProps={{
+                style: { borderRadius: '8px', fontFamily: 'Poppins' },
+              }}
+              sx={{ width: '100%' }}
+              id="email"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={Boolean(errors.email)}
+              helperText={errors.email}
+              label="Correo electronico"
+              placeholder="example@gmail.com"
+            />
 
-                label="Correo electronico"
-                placeholder='myriambregman@urss-mail.ru'/>
+            <TextField
+              sx={{ width: '100%' }}
+              id="password"
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={Boolean(errors.password)}
+              helperText={errors.password}
+              InputLabelProps={{ style: { fontFamily: 'Poppins' } }}
+              InputProps={{
+                style: { borderRadius: '8px', fontFamily: 'Poppins' },
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={() => {
+                        setFieldValue(
+                          'isPasswordVisible',
+                          !values.isPasswordVisible,
+                        );
+                      }}
+                    >
+                      {values.isPasswordVisible ? (
+                        <img src="assets/icon/eye.svg" alt="visibility" />
+                      ) : (
+                        <img
+                          src="assets/icon/eye-off.svg"
+                          alt="visibility-off"
+                        />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+              type={values.isPasswordVisible ? 'text' : 'password'}
+              label="Contraseña"
+              placeholder="********"
+            />
 
-              <TextField
-                sx={{ width: '100%' }}
-                
-                id="password"
-                value={values.password}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={Boolean(errors.password)}
-                helperText={errors.password}
-                
-                InputLabelProps={{ style: { fontFamily: 'Poppins' } }}
-                InputProps={{
-                  style: { borderRadius: '8px', fontFamily: 'Poppins' },
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label='toggle password visibility'
-                        onClick={() => {
-                          setFieldValue('isPasswordVisible', !values.isPasswordVisible);
-                        }}
-                      >
-                        {
-                          values.isPasswordVisible ?
-                          <img src='assets/icon/eye.svg' alt='visibility' /> :
-                          <img src='assets/icon/eye-off.svg' alt='visibility-off' />
-                        }
-                      </IconButton>
-                    </InputAdornment>
-                  )
-                }}
+            <Button type="submit" className="mt-4" isLoading>
+              Ingresar
+            </Button>
 
-                type={values.isPasswordVisible ? 'text' : 'password'}
-                label="Contraseña"
-                placeholder='********'/>
-              
-              <Button type='submit' className='mt-4' isLoading>Ingresar</Button>
-
-              {/* TODO: Desactivar el boton hasta las 21hs del día domingo 19 GMT-3 */}
-              <Link
-                to={paths.totalResults}
-                className="mt-4 p-[18px] w-full text-violet-brand underline rounded-xl">
-                Ver Escrutinio
-              </Link>
-            </Form>
-          )
-        }
+            {/* TODO: Desactivar el boton hasta las 21hs del día domingo 19 GMT-3 */}
+            <Link
+              to={paths.totalResults}
+              className="mt-4 p-[18px] w-full text-violet-brand underline rounded-xl"
+            >
+              Ver Escrutinio
+            </Link>
+          </Form>
+        )}
       </Formik>
     </main>
   );
