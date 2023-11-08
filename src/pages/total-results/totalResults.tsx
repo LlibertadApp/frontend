81fd428badcfbc486f56038f0d8930ad3e9005aa
AlhuayOscar <oscar_alhuay2001@hotmail.com>
2023-11-07 22:44:46 -0300

    Filters changed logic to adapt text input

--- src/pages/total-results/totalResults.tsx
+++ src/pages/total-results/totalResults.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { observer } from 'mobx-react';
 import { X, ArrowRight } from '@phosphor-icons/react';
 
-import { FilterPage } from '#/pages/filter-results/filterResults';
+import FilterPage from '#/pages/filter-results/filterResults';
 import { Filter, useFilter } from '#/context/FilterContext';
 import { ButtonFilter } from '#/components/buttonFilter';
 import { ButtonClearFilter } from '#/components/buttonClearFilter';
@@ -66,7 +66,10 @@ const TotalResultsPage = () => {
               clearFilters={clearFilters}
             />
           )}
-          <button className='w-full' onClick={() => setIsFilterMenuOpen(!isFilterMenuOpen)}>
+          <button
+            className="w-full"
+            onClick={() => setIsFilterMenuOpen(!isFilterMenuOpen)}
+          >
             <ButtonFilter amount={filters.length} />
           </button>
         </section>
