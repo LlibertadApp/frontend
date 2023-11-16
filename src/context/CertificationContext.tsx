1ed22e6a86fe14afc4b35041a7f9911106d36e83
AlhuayOscar <oscar_alhuay2001@hotmail.com>
2023-11-15 21:43:52 -0300

    Experimental UseEffect uploadCertificate redirection with loaded + localStorage image file exists

--- src/context/CertificationContext.tsx
+++ src/context/CertificationContext.tsx
@@ -1,26 +1,43 @@
 import React, { createContext, useContext, useState, ReactNode } from 'react';
 
 // Crear un contexto para el certificado
-const CertificateContext = createContext<{
-  file?: File
-  setFile: (file: File) => void
+const CertificateContext = createContext<
+  | {
+      file?: File;
+      setFile: (file: File) => void;
+      certificateImage?: File;
+      setCertificateImage: (image: File) => void;
+    }
+  | undefined
+>(undefined);
 
-  certificateImage?: File
-  setCertificateImage: (image: File) => void
-} | undefined>(undefined);
+export const CertificateProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [file, setFile] = useState<File | undefined>();
+  const [certificateImage, setCertificateImage] = useState<File | undefined>();
 
-export function CertificateProvider(
-  { children }: { children: ReactNode }
-) {
-  const [file, setFile] = useState<File>();
-  const [certificateImage, setCertificateImage] = useState<File>();
+  const storedPicture = localStorage.getItem('actualPicture');
+  if (storedPicture) {
+    fetch(storedPicture)
+      .then((response) => response.blob())
+      .then((blob) => {
+        setFile(blob as File);
+        setCertificateImage(blob as File);
+      })
+      .catch((error) => {
+        console.error('Error loading picture from localStorage:', error);
+      });
+  }
 
   return (
-    <CertificateContext.Provider value={{ file, setFile, certificateImage, setCertificateImage }}>
+    <CertificateContext.Provider
+      value={{ file, setFile, certificateImage, setCertificateImage }}
+    >
       {children}
     </CertificateContext.Provider>
   );
-}
+};
 
 // Crear un hook personalizado para acceder al contexto
 export const useCertificate = () => {
--- src/pages/upload-certificate/uploadCertificate.tsx
+++ src/pages/upload-certificate/uploadCertificate.tsx
@@ -1,13 +1,9 @@
-import { useEffect, useState } from 'react';
-
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { CheckCircle } from '@phosphor-icons/react';
-
 import ProgressIndicator from '#/components/progressIndicator';
 import Navbar from '#/components/navbar';
-import UploadImage from '#/components/uploadImage';
 import { ProgressStepStatus } from '#/components/progressIndicator/types';
-import './styles.css';
-import { useNavigate } from 'react-router-dom';
 import { useCertificate } from '#/context/CertificationContext';
 import { paths } from '#/routes/paths';
 import UploadInput from '#/components/uploadInput';
@@ -23,47 +19,81 @@ const CheckItem = ({ text }: { text: string }) => (
 
 const UploadCertificate = () => {
   const navigate = useNavigate();
-
-  // TODO: Replace with context useState
   const { setFile, setCertificateImage } = useCertificate();
 
   const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFile(e.target.files?.[0]!);
-    // setCertificateImage(URL.createObjectURL(e.target.files?.[0]!));
-    setCertificateImage(e.target.files?.[0]!); // test
+    const selectedFile = e.target.files?.[0];
 
-    navigate(paths.verifyCertificate);
-  }
+    setFile(e.target.files?.[0]!);
+    setCertificateImage(e.target.files?.[0]!);
+
+    if (selectedFile) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const fileDataUrl = e.target?.result as string;
+        localStorage.setItem('actualPicture', fileDataUrl);
+      };
+      reader.readAsDataURL(selectedFile);
+    }
+  };
+
+  // Verificar y navegar solo si actualPicture existe y tiene información
+  useEffect(() => {
+    const actualPicture = localStorage.getItem('actualPicture');
+    if (actualPicture) {
+      navigate(paths.verifyCertificate);
+    }
+  }, []);
 
   return (
     <>
       <Navbar routerLink={paths.home} />
-      <main className='container mx-auto p-4 flex flex-col gap-[30px] max-w-[52.5rem]'>
-        <ProgressIndicator steps={[ProgressStepStatus.Active, ProgressStepStatus.Pending, ProgressStepStatus.Pending]} />
-        <h1 className="text-neutral-700 text-xl font-medium text-center">Cargar imagen</h1>
-        <p className="text-neutral-600 text-base">Usá la cámara para subir <b>el certificado del fiscal</b>, o cargala desde la galería.</p>
-        <UploadInput 
-          id='largeCertificateInput' 
-          size='lg' 
-          onChange={handleImageUpload}/>
+      <main className="container mx-auto p-4 flex flex-col gap-[30px] max-w-[52.5rem]">
+        <ProgressIndicator
+          steps={[
+            ProgressStepStatus.Active,
+            ProgressStepStatus.Pending,
+            ProgressStepStatus.Pending,
+          ]}
+        />
+        <h1 className="text-neutral-700 text-xl font-medium text-center">
+          Cargar imagen
+        </h1>
+        <p className="text-neutral-600 text-base">
+          Usá la cámara para subir <b>el certificado del fiscal</b>, o cargala
+          desde la galería.
+        </p>
+        <UploadInput
+          id="largeCertificateInput"
+          size="lg"
+          onChange={handleImageUpload}
+        />
         <ul className="flex flex-col gap-[15px]">
           <li className="flex flex-row gap-[8px] text-left">
             <CheckCircle className="text-green" size={24} />
-            <span className="text-neutral-600 text-sm flex-1">Buscá un lugar con buena luz.</span>
+            <span className="text-neutral-600 text-sm flex-1">
+              Buscá un lugar con buena luz.
+            </span>
           </li>
           <li className="flex flex-row gap-[8px] text-left">
             <CheckCircle className="text-green" size={24} />
-            <span className="text-neutral-600 text-sm flex-1">Asegurate de que se vean todos los datos.</span>
+            <span className="text-neutral-600 text-sm flex-1">
+              Asegurate de que se vean todos los datos.
+            </span>
           </li>
           <li className="flex flex-row gap-[8px] text-left">
             <CheckCircle className="text-green" size={24} />
-            <span className="text-neutral-600 text-sm flex-1">Asegurate que el certificado esté firmado por el presidente de tu mesa.</span>
+            <span className="text-neutral-600 text-sm flex-1">
+              Asegurate que el certificado esté firmado por el presidente de tu
+              mesa.
+            </span>
           </li>
         </ul>
-        <UploadInput 
-          id='buttonCertificateInput' 
-          size='md' 
-          onChange={handleImageUpload}/>
+        <UploadInput
+          id="buttonCertificateInput"
+          size="md"
+          onChange={handleImageUpload}
+        />
       </main>
     </>
   );
