a1839d6efefeb48c8aceee074a33e8db66c58660
Nicolas Brusco <nicolasbrusco@hotmail.com>
2023-11-17 00:19:24 -0300

    adjustments for merging

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -8,7 +8,25 @@ const LoginPage: React.FC = () => {
     setError(true);
   }, 2000);
 
-  return !error ? (
+  return error ? (
+    <>
+      <div className="lg:absolute lg:inset-0 lg:bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-white to-red-error lg:z-0"></div>
+      <main className="px-4 mx-auto flex flex-col items-center rounded-3xl max-w-md gap-8 mt-20 lg:max-w-[624px] lg:px-[108px] lg:py-[50px] lg:relative lg:bg-white lg:mt-52">
+        <h1 className="text-3xl text-text-off lg:text-[38px] font-light text-center lg:leading-10">
+          La sesión
+          <br />
+          <strong className="text-red font-semibold">
+            no es válida o está vencida.
+          </strong>
+        </h1>
+        <img
+          src="assets/logos/fenix-new-bg-red.svg"
+          alt="fenix"
+          className="object-cover h-auto w-28 lg:w-40 mb-10 shadow-rose-600 shadow-md rounded-full bg-red-error"
+        />
+      </main>
+    </>
+  ) : (
     <>
       <div className="lg:absolute lg:inset-0 lg:bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-white to-violet-primary lg:z-0"></div>
       <main className="px-4 mt-52 mx-auto flex flex-col items-center max-w-md lg:max-w-[624px] lg:px-[108px] lg:py-[50px] rounded-3xl lg:relative lg:bg-white">
@@ -32,24 +50,6 @@ const LoginPage: React.FC = () => {
             identidad...
           </strong>
         </h1>
-      </main>
-    </>
-  ) : (
-    <>
-      <div className="lg:absolute lg:inset-0 lg:bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-white to-red-error lg:z-0"></div>
-      <main className="px-4 mx-auto flex flex-col items-center rounded-3xl max-w-md gap-8 mt-20 lg:max-w-[624px] lg:px-[108px] lg:py-[50px] lg:relative lg:bg-white lg:mt-52">
-        <h1 className="text-3xl text-text-off lg:text-[38px] font-light text-center lg:leading-10">
-          La sesión
-          <br />
-          <strong className="text-red font-semibold">
-            no es válida o está vencida.
-          </strong>
-        </h1>
-        <img
-          src="assets/logos/fenix-new-bg-red.svg"
-          alt="fenix"
-          className="object-cover h-auto w-28 lg:w-40 mb-10 shadow-rose-600 shadow-md rounded-full bg-red-error"
-        />
       </main>
     </>
   );
